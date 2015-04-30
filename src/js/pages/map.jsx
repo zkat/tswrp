aaef8622dccaab29a7a2e133ea96a3c428c4437b
Kat Marchán <kzm@sykosomatic.org>
2015-04-29 22:38:41 -0700

    WIP new map features

--- src/js/pages/map.jsx
+++ src/js/pages/map.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import ReactFireMixin from "reactfire";
 
 export default React.createClass({
+  mixins: [ReactFireMixin],
   componentDidMount() {
-    this.props.mapIdBus.push("5b1507cf5922f6b03de3d00d41394a81");
+    this.setMapUrl(this.props.element);
+  },
+  componentWillReceiveProps(props) {
+    this.setMapUrl(props.element);
+  },
+  setMapUrl(element) {
+    let baseUrl = "5b1507cf5922f6b03de3d00d41394a81";
+    let path = element ? "#tswrp/${element.id}" : "";
+    this.props.mapIdBus.push(`${baseUrl}${path}`);
   },
   render() {
     return <div/>;
